6e1a61cd3810b4b0f25bbad0b2b626443dca7eca
Allen Cho <allencho@uw.edu>
2018-01-11 21:09:30 -0800

    break

---
+++ nbaPlayer.php
@@ -0,0 +1,23 @@
+class NBAPlayer {
+        public -> $name = "";
+        public -> $team = "";
+        public -> $gp = "";
+        public -> $min = "";
+        public -> $mfg = "";
+        public -> $afg = "";
+        public -> $pctfg = "";
+        public -> $m3pt = "";
+        public -> $a3pt = "";
+        public -> $pct3pt = "";
+        public -> $mft = "";
+        public -> $aft = "";
+        public -> $pctft = "";
+        public -> $offrb = "";
+        public -> $defrb = "";
+        public -> $afg = "";
+        public -> $afg = "";
+        public -> $afg = "";
+        public -> $afg = "";
+        public -> $afg = "";
+        
+}

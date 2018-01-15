c83c06ad92271add9d1596e72ccc3e5ea7f9993d
unknown <iGuest@is-m440m27-22.ischool.uw.edu>
2018-01-15 01:35:26 -0800

    simple function work

--- PA1/databaseAccess.php
+++ PA1/databaseAccess.php
@@ -1,63 +1,29 @@
 
 <?php
-function listOfSearchingPlayers($inputFromUser) {
-        $username = 'info344user';
-        $password = '<password>';
 
-        try {
-            $conn = new PDO('mysql:host=acho2dbinstance.cmlvjggv28ym.us-west-2.rds.amazonaws.com;dbname=CSV_DB', $username, $password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
-            
-            $temp = $conn->prepare('SELECT NAME FROM NBAPlayerList');
-            $temp-> execute();
-            $playerNameArray= array();
-            while($row = $temp->fetch()) {
-                $split = explode(" ", $row[0]);
-                array_push($playerNameArray, $split[0]);
-                array_push($playerNameArray, $split[1]);
+    echo levenshtein('secx', "sangchul");
+    $username = 'info344user';
+    $password = '<password>';
+
+    try {
+        $conn = new PDO('mysql:host=acho2dbinstance.cmlvjggv28ym.us-west-2.rds.amazonaws.com;dbname=CSV_DB', $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
+        
+        
+        if (isset($_GET['playerName'])) {
+            $input = $_GET['playerName'];
+            $stmt = $conn->prepare("SELECT * FROM NBAPlayerList WHERE Name LIKE '%$input%'");
+            $stmt->execute();
+            $playerList = $stmt->fetchAll();
+            include 'nbaPlayer.php';
+            foreach($playerList as $eachPlayer) {
+                $player = new Player($eachPlayer[0], $eachPlayer[2], $eachPlayer[1], $eachPlayer[21], $eachPlayer[7], $eachPlayer[15], $eachPlayer[16]);
+                echo $player -> getInfo();
             }
-
-            // if(isset($_GET['playerName'])) {
-            //     $input = $_GET['playerName'];
-                $input = $inputFromUser;
-                $closest = "";
-                $shortest = -1;
-                foreach($playerNameArray as $playerName) {
-                    $lev = levenshtein($input, $playerName);
-                    if($lev == 0) {
-                        $closest = $playerName;
-                        $shortest = 0;
-                        break;
-                    }
-                    if ($lev <= $shortest || $shortest < 0) {
-                        $closest  = $playerName;
-                        $shortest = $lev;
-                    }
-                }
-
-
-                $stmt = $conn->prepare("SELECT * FROM NBAPlayerList WHERE Name LIKE '%$input%' or Name LIKE '%$closest%'");
-                //$stmt->bind_param("s", $_GET['playerName']);
-                $stmt->execute();
-                $players = $stmt->fetchAll();
-                $playersResult = array();
-                
-                include 'nbaPlayer.php';
-                foreach($players as $player) {
-                    $eachPlayer = new Player($player[0], $player[1], $player[2], $player[3], $player[4],
-                    $player[5], $player[6], $player[7], $player[8], $player[9], $player[10], $player[11], $player[12],
-                    $player[13], $player[14], $player[15], $player[16], $player[17], $player[18], $player[19], $player[20],
-                    $player[21]);
-                    array_push($playersResult, $eachPlayer);
-                }
-                echo "CHECKING DATABASEACCESS.PHP";
-                print_r(array_values($playersResult));
-                
-                //return $playersResult;
-            //}
-            
-        } catch(PDOException $e) {
-            echo 'ERROR: ' . $e->getMessage();
         }
+        
+    } catch(PDOException $e) {
+        echo 'ERROR: ' . $e->getMessage();
     }
+
 ?>
--- PA1/nba.com.php
+++ PA1/nba.com.php
@@ -5,16 +5,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>NBA PA1</title>
+
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+    <style>
+        nav {
+            margin-bottom: 50px;
+        }
+    </style>
+    
 </head>
 <body>
+    
+    <div class="container">
+        <nav id="navigation" class="z-depth-5">
+            <div class="nav-wrapper">
+            <a href="#" class="brand-logo">NBA Player List</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="sass.html">Home</a></li>
+                <li><a href="badges.html">Contacts</a></li>
+                <li><a href="collapsible.html">About</a></li>
+            </ul>
+            </div>
+        </nav>
+        <form action="nba.com.php" method ='GET'>
+            <div class="input-field">
+                <input placeholder="Name of your NBA player" type="text" id="playerName" name="playerName">
+                <label class="active" for="name">Player</label>
+                <input class="btn waves-effect" type="submit" value="Submit">
+            </div>
+            
+        </form>
+        
+        <?php
+            include 'databaseAccess.php';
+        ?>
+    </div>
 
-    <form action="form.php" method ='GET'>
-        <p>Search Your Player: <input type='text' name="playerName"><input type="submit" value="Submit"></p>
-    </form>
-    <?php
-        include 'form.php';
-        //include 'databaseAccess.php';
-
-    ?>
+    
+    
 </body>
 </html>
